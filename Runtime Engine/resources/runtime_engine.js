a62bd365788eb5f95b6f604bb74dbac2cded50f1
pinoodle <103074631+pinoodle@users.noreply.github.com>
2025-07-24 21:31:18 +0100

    I'm confused...

--- Runtime Engine/resources/runtime_engine.js
+++ Runtime Engine/resources/runtime_engine.js
@@ -349,22 +349,7 @@ function onGameLoad() {
       keys = [];
     }
 
-    /* // ROOM AND INVENTORY
-    else if (event.key === "Shift") {
-      if (document.getElementById("stats_btn").classList.contains("active_bottom_btn") || document.getElementById("settings_btn").classList.contains("active_bottom_btn")) dom_scene.click();
-      document.getElementById("surroundings_btn").click();
-    }
-
-    else if (event.key === "Control") {
-      if (ctrl > 0) {
-        if (document.getElementById("stats_btn").classList.contains("active_bottom_btn") || document.getElementById("settings_btn").classList.contains("active_bottom_btn")) dom_scene.click();
-        document.getElementById("inventory_btn").click();
-      }
-    } */
-
   });
-
-  // var ctrlFix = setTimeout(function() { ctrl++; }, 1000);
 
   try { definitions[room](); } catch(error) { print(error); }
   changeColor(kolorek);
